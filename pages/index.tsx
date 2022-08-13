7f2d4505a44298c1a3ffdca3b2b1d66ba4a63b12
iammurali <muralismail4u@gmail.com>
2022-08-13 16:25:14 +0530

    🐛 FIX: build

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import { GithubCalendar } from "./components/github";
-import Hero from "./components/hero";
-import NavBar from "./components/navbar";
-import RecentBlogs from "./components/projects";
+import { GithubCalendar } from "../components/github";
+import Hero from "../components/hero";
+import NavBar from "../components/navbar";
+import RecentBlogs from "../components/projects";
 
 const Home: NextPage = () => {
   return (
