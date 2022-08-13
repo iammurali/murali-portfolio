e45426668443142ab1aee523dea9752da3a6bf61
iammurali <muralismail4u@gmail.com>
2022-08-13 16:16:54 +0530

    🐛 FIX:

--- pages/components/hero.tsx
+++ pages/components/hero.tsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="text-gray-400 bg-black body-font">
       <div className="max-w-7xl  sm:px-6 lg:px-8 container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <p className="mb-4 leading-relaxed">I'm MURALI</p>
+          <p className="mb-4 leading-relaxed">Im MURALI</p>
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
             Your friendly neighborhood <br/>
             <p className="text-indigo-400">Developer</p>
