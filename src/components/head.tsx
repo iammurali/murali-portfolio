21ec08991471bde639eb0b67da9bdb0065ba8f17
muralielumalai <murali.elumalai@lilly.com>
2023-06-25 15:54:38 +0530

    change title

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -3,7 +3,7 @@ import Head from "next/head";
 export default function Header() {
   return (
     <Head>
-      <title>ME</title>
+      <title>Murali</title>
       <meta name="description" content="Murali elumalai portfolio" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="icon" href="/favicon.ico" />
