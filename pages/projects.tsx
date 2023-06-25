29401ba535c236660ce1433fc82574841451d8b0
muralielumalai <murali.elumalai@lilly.com>
2023-06-25 16:41:09 +0530

    fix responsiveness

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -17,14 +17,14 @@ export default function About() {
         I will keep building stuff that will use and list out here`}
       </p>
       <hr className="border-neutral-500 mt-3 mb-3"></hr>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <a
           rel="noopener noreferrer"
           target="_blank"
           href={`https://github.com/iammurali/finnacle`}
           className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
         >
-          <div>
+          <div className="flex flex-col flex-1">
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <GithubIcon />
@@ -44,7 +44,7 @@ export default function About() {
           href={`https://github.com/iammurali/snuglweb`}
           className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
         >
-          <div>
+          <div className="flex flex-col flex-1">
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <GithubIcon />
@@ -64,7 +64,7 @@ export default function About() {
           href={`https://github.com/iammurali/animated-portfolio`}
           className="flex w-full border border-neutral-200 dark:border-neutral-800 rounded-lg p-4 no-underline items-center text-neutral-800 dark:text-neutral-200 hover:dark:bg-neutral-900 hover:bg-neutral-100 transition-all justify-between"
         >
-          <div>
+          <div className="flex flex-col flex-1">
             <div className="flex items-center justify-between">
               <div className="flex items-center">
                 <GithubIcon />
